289cd33459f42f04caa9c760dd17fc2b5d417045
amy <amyniovi@gmail.com>
2015-11-19 19:36:25 +0000

    Custom github angular service

--- AngularJSStuff/Scripts/Services/Github.js
+++ AngularJSStuff/Scripts/Services/Github.js
@@ -1,5 +1,28 @@
 ﻿(function () {
+    //get a reference to the ng-app
+    var module = angular.module("githubviewer");
 
+    var github = function ($http) {
 
+        var getUser = function (username) {
+            return $http.get("https://api.github.com/users/" + username)
+                .then(function (response) { return response.data; });
+        };
+    
+        var getRepos = function (user) {
+            return $http.get(user.repos_url)
+            .then(function (response) { return response.data;});
+        };
+
+        //public API obj
+        return {
+            getUser: getUser,
+            getRepos: getRepos
+        };
+
+        //register service with angular
+        module.factory("github", github)
+
+    };
 
 })();
