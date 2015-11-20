9ac48b39b1fbb8d631f7b1d64ab1bc7f58b22a3c
amy <amyniovi@gmail.com>
2015-11-20 09:49:24 +0000

    Showing User s Repositories

--- AngularJSStuff/Scripts/Controllers/MainController.js
+++ AngularJSStuff/Scripts/Controllers/MainController.js
@@ -4,13 +4,24 @@
     var module = angular.module("githubviewer");
 
     var MainController = function ($scope, github) {
-
+       
         var onUserComplete = function (data) {
             $scope.user = data;
+            github.getRepos($scope.user).then(onReposComplete, onReposError);
         };
 
         var onUserError = function (data) {
+            $scope.user = null;
             $scope.error = "Oops...could not retrieve this guy...";
+        };
+
+        var onReposError = function (data) {
+            $scope.reposError = "Could not fetch repos....";
+        };
+
+        var onReposComplete = function (data) {
+            $scope.repos = data;
+           
         };
 
         $scope.Search = function (username) {
@@ -18,8 +29,7 @@
                 .then(onUserComplete, onUserError);
         };
 
-        $scope.message = "Hey angular...";
-
+    
     };
 
     module.controller("MainController", MainController);
